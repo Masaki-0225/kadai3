39be2b8a014bece1f420203b0bf463f74738d3cd
hirakawa masaki <hirakawamasaki@hirakawanoiMac.local>
2024-02-06 22:41:13 +0900

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Name> nameList = new ArrayList<>();
+        nameList.add(new Name("ジャック", 24));
+        nameList.add(new Name("マーカス", 35));
+        nameList.add(new Name("平川", 32));
+
+        for (int i = 0; i < 5; i++) {
+            try {
+                if (i < 0 || nameList.size() <= i) {
+                    throw new ArrayIndexOutOfBoundsException();
+                }
+                System.out.println(nameList.get(i));
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("この数値は存在しません；" + i);
+            }
+
+        }
+    }
+}
