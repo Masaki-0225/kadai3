a609131d1c0bcbc9da0eab9e0f519f333b601c4e
hirakawa masaki <hirakawamasaki@hirakawanoiMac.local>
2024-03-23 01:41:42 +0900

    memberを追加

---
+++ src/main/java/org/example/Member.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Member {
+    private String name;
+    private String ult;
+
+    public Member(String name, String ult) {
+        this.name = name;
+        this.ult = ult;
+    }
+}
